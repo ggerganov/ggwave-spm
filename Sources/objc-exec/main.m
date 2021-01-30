1df30da2f82c6b447a418dfd83062380a2edd940
Georgi Gerganov <ggerganov@gmail.com>
2021-01-30 10:18:46 +0200

    wip : examples

--- Sources/objc-exec/main.m
+++ Sources/objc-exec/main.m
@@ -1,11 +1,41 @@
-#import <Foundation/Foundation.h>
-
 #import "ggwave/ggwave.h"
 
+#import <Foundation/Foundation.h>
+
+#define CHECK(cond) \
+    if (!(cond)) { \
+        NSLog(@"[%s:%d] Check failed: %s\n", __FILE__, __LINE__, #cond); \
+        exit(1); \
+    }
+
+#define CHECK_T(cond) CHECK(cond)
+#define CHECK_F(cond) CHECK(!(cond))
+
+
 int main() {
-    int result = 10;
     ggwave_Parameters parameters = ggwave_getDefaultParameters();
 
-    printf("Hello from Objc %d %d", result, parameters.samplesPerFrame);
+    parameters.sampleFormatInp = GGWAVE_SAMPLE_FORMAT_I16;
+    parameters.sampleFormatOut = GGWAVE_SAMPLE_FORMAT_I16;
+
+    ggwave_Instance instance = ggwave_init(parameters);
+
+    int ret;
+    const char * payload = "test";
+    char decoded[256];
+
+    int n = ggwave_encode(instance, payload, 4, GGWAVE_TX_PROTOCOL_AUDIBLE_FAST, 50, NULL, 1);
+    char waveform[n];
+
+    ret = ggwave_encode(instance, payload, 4, GGWAVE_TX_PROTOCOL_AUDIBLE_FAST, 50, waveform, 0);
+    CHECK(ret > 0);
+
+    ret = ggwave_decode(instance, waveform, sizeof(int16_t)*ret, decoded);
+    CHECK(ret == 4);
+
+    CHECK(strcmp(decoded, payload) == 0);
+
+    ggwave_free(instance);
+
     return 0;
 }
--- Sources/swift-exec/main.swift
+++ Sources/swift-exec/main.swift
@@ -1,3 +1,16 @@
 import ggwave
 
-print("Hello from Swift \(ggwave_getDefaultParameters())")
+var parameters = ggwave_getDefaultParameters()
+
+parameters.sampleFormatInp = GGWAVE_SAMPLE_FORMAT_I16
+parameters.sampleFormatOut = GGWAVE_SAMPLE_FORMAT_I16
+
+let instance = ggwave_init(parameters)
+
+let payload = "test"
+let decoded: Array<Character> = Array(repeating: "\0", count: 256)
+
+let n = ggwave_encode(instance, payload, 4, GGWAVE_TX_PROTOCOL_AUDIBLE_FAST, 50, nil, 1)
+let waveform: Array<Character> = Array(repeating: "\0", count: Int(n))
+
+let ret = ggwave_encode(instance, payload, 4, GGWAVE_TX_PROTOCOL_AUDIBLE_FAST, 50, waveform, 0);
